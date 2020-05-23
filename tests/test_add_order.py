b57c1d62216c5e86ba92f59a0746553fafbfa003
Cristian Vergel <cristianvergel5@gmail.com>
2020-05-23 13:13:35 -0500

    Test add order complete

---
+++ tests/test_add_order.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import pytest
+from intelDeliveryBot import intelDelivery
+import numpy as np
+
+
+
+@pytest.fixture
+def intel_delivery_bot():
+    return intelDelivery()
+
+@pytest.mark.parametrize('username,password,expected', [
+    ('cristianvergel', 'cristianvergel', True),  
+])
+
+def test_add_order(intel_delivery_bot, username, password, expected):
+    login_status = intel_delivery_bot.singIn(username, password)
+    login_status = intel_delivery_bot.addOrder()
+    assert login_status == expected
+    
